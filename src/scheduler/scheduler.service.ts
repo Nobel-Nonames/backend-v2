f403c726441fd075bb6ccefb399d30dfc59c8c2b
th-release <cth.release@gmail.com>
2023-12-28 19:48:57 +0900

    feat: scheduler

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import SystemEntity from 'src/entitiy/system.entity';
 import { SystemService } from 'src/system/system.service';
 
 @Injectable()
@@ -12,15 +13,14 @@ export class SchedulerService {
   async updateEvent() {
     const last = await this.systemService.findOneByLastData();
     if (!last) {
-      await this.systemService.systemCreate({
-        event_number: 1,
-        createdAt: new Date()
-      });
+      const last = new SystemEntity();
+      last.event_number = 1;
+      await this.systemService.systemSave(last);
 
       return
     } else {
       last.event_number += 1;
-      await this.systemService.systemDocSave(last);
+      await this.systemService.systemSave(last);
 
       return
     }
